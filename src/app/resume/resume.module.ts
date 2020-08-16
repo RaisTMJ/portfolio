20c3ba8e7bc91a3fb0d56cfb167713a0aba3a6a6
hannah.mazri <hannah.mazri@terang.asia>
2020-08-16 16:14:41 +0800

    feat: init resume

---
+++ src/app/resume/resume.module.ts
@@ -0,0 +1,13 @@
+import { ResumeComponent } from './resume.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [ ResumeComponent ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ResumeModule { }
